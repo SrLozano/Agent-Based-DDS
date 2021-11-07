35f822a6c2b17032f4ae30989ad71617e41492d8
Sergi Cirera-Rocosa <72690371+Sergi-Cirera-Rocosa@users.noreply.github.com>
2021-11-07 15:45:08 +0100

    Read parameters of classifiers

--- src/main/java/agents/classifierAgent.java
+++ src/main/java/agents/classifierAgent.java
@@ -3,18 +3,26 @@ package agents;
 import jade.core.Agent;
 //import jade.core.Filter;
 import weka.classifiers.Evaluation;
+import weka.core.SerializationHelper;
 import weka.core.converters.ConverterUtils.DataSource;
 import weka.core.Instances;
 import weka.classifiers.trees.J48;
 import weka.filters.unsupervised.instance.RemovePercentage;
 import weka.filters.Filter;
+
+import java.io.FileInputStream;
+import java.io.ObjectInputStream;
 import java.util.Random;
+
+
 
 public class classifierAgent extends Agent {
     public static void main(String[] args) throws Exception {
 
         double percentage = 6.5;
 
+
+        // Read the dataset for user or coord agent
         DataSource source = new DataSource(System.getProperty("user.dir") + "/audit_risk.arff");
         if(source != null){
             System.out.println("Read");
@@ -23,7 +31,7 @@ public class classifierAgent extends Agent {
             Random random = new Random(42);
             data.randomize(random);
 
-            // Slip between train and test
+            // Split between train and test for coord agent
             RemovePercentage rp = new RemovePercentage();
             rp.setInputFormat(data);
             rp.setPercentage(percentage);
@@ -48,21 +56,32 @@ public class classifierAgent extends Agent {
             if (train.classIndex() == -1) {
                 train.setClassIndex(train.numAttributes() - 1);
             }
-
-            J48 classifier = new J48();
-
-            classifier.buildClassifier(train);
-
-            weka.core.SerializationHelper.write(System.getProperty("user.dir") + "/classifier.model", classifier);
-
             // Setting class attribute if the data format does not provide this information
             if (test.classIndex() == -1) {
                 test.setClassIndex(test.numAttributes() - 1);
             }
 
+
+            // Train classifier and save it
+            J48 classifier = new J48();
+            classifier.buildClassifier(train);
+            weka.core.SerializationHelper.write(System.getProperty("user.dir") + "/classifier.model", classifier);
+
+
+            // Test classifier
             Evaluation eval = new Evaluation(test);
             eval.evaluateModel(classifier, test);
             System.out.println((eval.correct()/test.numInstances())*100);
+
+
+
+            // Loading an already saved tree
+            J48 treeClassifier = (J48) SerializationHelper.read(new FileInputStream(System.getProperty("user.dir") + "/classifier.model"));
+
+            Evaluation eval2 = new Evaluation(test);
+            eval.evaluateModel(treeClassifier, test);
+            System.out.println((eval2.correct()/test.numInstances())*100);
+
         }
     }
 }
